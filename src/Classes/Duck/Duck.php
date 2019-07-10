e4b8064da654c045c68e288a7d9263fcc2a943a1
glaphire <motoryna.dariia@pdffiller.team>
2019-07-10 23:28:23 +0300

    created abstract class for ducks

---
+++ src/Classes/Duck/Duck.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Classes\Duck;
+
+
+use App\Interfaces\FlyBehavior;
+use App\Interfaces\QuackBehavior;
+
+abstract class Duck
+{
+    protected $quackBehavior;
+
+    protected $flyBehavior;
+
+    public function __construct(QuackBehavior $quackBehavior, FlyBehavior $flyBehavior)
+    {
+        $this->quackBehavior = $quackBehavior;
+        $this->flyBehavior = $flyBehavior;
+    }
+
+    public function performFly()
+    {
+        $this->flyBehavior->fly();
+    }
+
+    public function performQuack()
+    {
+        $this->quackBehavior->quack();
+    }
+
+    public function swim()
+    {
+        print_r("All ducks float, even decoys.");
+    }
+
+    abstract public function display();
+}
