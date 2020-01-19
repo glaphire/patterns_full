3f50d85428be48f263565971c75790ea7fffd201
glaphire <motoryna.dariia@pdffiller.team>
2020-01-19 23:24:17 +0200

    finished PoC

--- src/Application.php
+++ src/Application.php
@@ -2,10 +2,30 @@
 
 namespace App;
 
+use App\Classes\WordsCollection;
+
 class Application
 {
     public function run()
     {
-        echo "something is running here\n";
+        $collection = new WordsCollection();
+        $collection->addItem("First");
+        $collection->addItem("Second");
+        $collection->addItem("Third");
+        $collection->addItem("Fourth");
+        
+        echo "Straight traversal:\n";
+
+        foreach ($collection->getIterator() as $item) {
+            echo $item . PHP_EOL;
+        }
+        
+        echo PHP_EOL;
+        
+        echo "Reverse traversal:\n";
+
+        foreach ($collection->getReverseIterator() as $item) {
+            echo $item . PHP_EOL;
+        }
     }
 }
