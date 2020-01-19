f8907ae78b30d9f04bcd5b902673f7df5ce426ed
glaphire <motoryna.dariia@pdffiller.team>
2020-01-19 23:03:43 +0200

    implemented iterator aggregate

---
+++ src/Classes/WordsCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Classes;
+
+class WordsCollection implements \IteratorAggregate
+{
+    private $items = [];
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function addItem($item)
+    {
+        $this->items[] = $item;
+    }
+
+    public function getIterator(): \Iterator
+    {
+        return new AlphabeticalOrderIterator($this);
+    }
+
+    public function getReverseIterator(): \Iterator
+    {
+        return new AlphabeticalOrderIterator($this, true);
+    }
+}
