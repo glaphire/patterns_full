e9925a4eea850cbdc530d57aa2d3a63ecf141541
glaphire <motoryna.dariia@pdffiller.team>
2020-01-29 19:12:15 +0200

    wrote test for builder

--- src/Tests/DirectorTest.php
+++ src/Tests/DirectorTest.php
@@ -1,10 +1,29 @@
 <?php
 
-
 namespace App\Tests;
 
+use App\Classes\Parts\Car;
+use App\Classes\CarBuilder;
+use App\Classes\Director;
+use App\Classes\Parts\Truck;
+use App\Classes\TruckBuilder;
+use PHPUnit\Framework\TestCase;
 
-class DirectorTest
+class DirectorTest extends TestCase
 {
+    public function testCanBuildTruck()
+    {
+        $truckBuilder = new TruckBuilder();
+        $newVehicle = (new Director())->build($truckBuilder);
 
+        $this->assertInstanceOf(Truck::class, $newVehicle);
+    }
+
+    public function testCanBuildCar()
+    {
+        $carBuilder = new CarBuilder();
+        $newVehicle = (new Director())->build($carBuilder);
+
+        $this->assertInstanceOf(Car::class, $newVehicle);
+    }
 }
